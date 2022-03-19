373846ecc0bd56f079f2768c9889fbf892be1e7e
Apple <apple@MacBook-Air.local>
2022-03-19 23:40:16 +0530

    source map error resolved by replacing react-scripts-v5.0.0 to vv4.0.3

---
+++ client/webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          enforce: "pre",
+          use: ["source-map-loader"],
+        },
+      ],
+    },
+    ignoreWarnings: [/Failed to parse source map/],
+  };
+  
